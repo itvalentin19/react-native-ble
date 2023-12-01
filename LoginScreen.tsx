262ea0d8f82d1b655d1ea43df53ae5d6f8873502
Lucian Valentin <itvalentin19@gmail.com>
2023-12-01 14:49:48 +0200

    Fix login screen key board issue

--- LoginScreen.tsx
+++ LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, Image, StyleSheet, Alert } from 'react-native';
+import { View, TextInput, Button, Text, Image, StyleSheet, Alert, SafeAreaView, KeyboardAvoidingView, ScrollView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as authService from './auth_service';
 import constants from './constants';
@@ -11,7 +11,7 @@ const LoginScreen: React.FC = () => {
   const navigation = useNavigation();
 
   const handleLogin = () => {
-    if (email == '' || pin == '') {return;}
+    if (email == '' || pin == '') { return; }
     authService.login(email, pin).then(async res => {
       if (res.error == false) {
         await authService.saveToken(res.data)
@@ -25,33 +25,37 @@ const LoginScreen: React.FC = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={{ alignItems: 'center' }}>
-        <Image source={{ uri: constants.APP_LOGO }} style={styles.logo} />
-      </View>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        keyboardType={'email-address'}
-        onChangeText={setEmail}
-        style={styles.textInput}
-      />
-      <TextInput
-        placeholder="PIN"
-        value={pin}
-        keyboardType={'number-pad'}
-        onChangeText={setPin}
-        style={styles.textInput}
-      />
-      <Button title="Login" onPress={handleLogin} />
-    </View>
+    <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={{ alignItems: 'center' }}>
+            <Image source={{ uri: constants.APP_LOGO }} style={styles.logo} />
+          </View>
+          <TextInput
+            placeholder="Email"
+            value={email}
+            keyboardType={'email-address'}
+            onChangeText={setEmail}
+            style={styles.textInput}
+          />
+          <TextInput
+            placeholder="PIN"
+            value={pin}
+            keyboardType={'numeric'}
+            onChangeText={setPin}
+            style={styles.textInput}
+          />
+          <Button title="Login" onPress={handleLogin} />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20, gap: 5, backgroundColor: 'white' },
-  logo: { width: 100, height: 100, resizeMode: 'contain', marginVertical: 20 },
-  textInput: { height: 40, borderWidth: 1, borderColor: 'gray', marginVertical: 10, padding: 10, borderRadius: 4 }
+  container: { flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 40, gap: 5, backgroundColor: 'white' },
+  logo: { width: 100, height: 100, resizeMode: 'contain', marginVertical: 50 },
+  textInput: { height: 40, borderWidth: 1, borderColor: 'gray', marginVertical: 10, padding: 10, borderRadius: 8 }
 })
 
 export default LoginScreen;
